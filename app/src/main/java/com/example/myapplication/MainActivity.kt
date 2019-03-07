f17e727caed47168fd3e886e7f16d90776b8bd60
gaoxiaowei <gaoxiaoweiandy@163.com>
2019-03-07 18:03:24 +0800

    练习到use when

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,40 @@
 package com.example.myapplication
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
-
     companion object {
         var TAG = "MainActivity";
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         var len = getStringLength("GAOXIAOWEI");
         Log.e(TAG,"strLen="+len);
         var strArray:Array<String> = arrayOf("hgf","rnm");
        //gxw-  printWhileArray(strArray);
         printForArray(strArray);
-
         checkIn();
+       // when多条件选择的用法，类似于switch
+        cases("Hello")
+        cases(1)
+        cases(0L)
+        cases(MainActivity())
+        cases("hello")
     }
 
+    //when多条件选择的用法，类似于switch
+    private fun cases(obj: Any) {
+        when (obj) {
+             1 -> Log.i(TAG, "1")
+            "Hello" -> Log.i(TAG, "Hello")
+            !is String ->Log.i(TAG, "obj is not String");
+            "hello" -> Log.i(TAG, "hello")
+            is Long ->Log.i(TAG, "Long")
+         else
+             -> Log.i(TAG, "unknown");
+      }
+    }
     //智能转换
     fun getStringLength(obj: Any): Int? {
         if (obj is String) {
@@ -31,8 +42,6 @@ class MainActivity : AppCompatActivity() {
         }
         return null;
     }
-
-
     //while循环
     fun printWhileArray( strArray:Array<String>)
     {
@@ -43,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             i++;
         }
     }
-
     //for循环
     fun printForArray( strArray:Array<String>)
     {
@@ -59,7 +67,6 @@ class MainActivity : AppCompatActivity() {
             Log.i(TAG,"strArray2[$j]="+strArray[j]);
         }
     }
-
     //in检查范围
     fun checkIn()
     {
@@ -68,7 +75,41 @@ class MainActivity : AppCompatActivity() {
         {
             Log.i(TAG,"a="+a);
         }
+        //判断是否在范围内
+        var y = 4;
+        if(y in 0..5)
+        {
+            Log.i(TAG,"1 in 0..5");
+        }
+        else
+        {
+            Log.i(TAG,"y is not in 0..5");
+        }
+        //判断一个数组是否包含该字符串
+        var aStr = "dd";
+        var strArray = arrayListOf<String>();
+        strArray.add("aaa");
+        strArray.add("bbb");
+        strArray.add("ccc");
 
+        if(aStr in strArray)
+        {
+            Log.i(TAG,"aStr in strArray");
+        }
+        else
+        {
+            Log.i(TAG,"aStr is not in strArray");
+        }
 
+        var index = 3;
+
+        if(index in 0..strArray.size - 1)
+        {
+            Log.i(TAG,"index is avilable");
+        }
+        else
+        {
+            Log.i(TAG,"index is out");
+        }
     }
 }
